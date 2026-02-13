eb714247dfd3798fc96f59bf2104db81f6eeb763
gajulapallepavankumarreddy@gmail.com <gajulapallepavankumarreddy@gmail.com>
2026-02-13 16:08:38 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("WELCOME TO PALINDROME CHECKER MANAGEMENT SYSTEM");
+        System.out.println("VERSION 1.0");
+        System.out.println("SYSTEM INITIALIZED SUCCESSFULLY");
+
+    }
+}
