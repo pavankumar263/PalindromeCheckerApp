c5b7e003d1db701be40f0050ea534213ae5bfdcc
pavankumar263 <gajulapallepavankumarreddy@gmail.com>
2026-03-09 15:45:43 +0530

    UC6

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,27 +1,34 @@
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 // Standardize input
 String cleanInput = original.toLowerCase();
 
-        // UC5 Logic: Use stack to reverse characters and validate
+        // UC6 Logic: Demonstrate FIFO vs LIFO
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-// 1. Push characters into stack
+// 1 & 2. Push characters to stack and enqueue characters
 for (int i = 0; i < cleanInput.length(); i++) {
-        stack.push(cleanInput.charAt(i));
-        }
+        char c = cleanInput.charAt(i);
+    stack.push(c);
+    queue.add(c); // add() acts as enqueue in Java
+}
 
         boolean isPalindrome = true;
 
-// 2. Pop and compare
-for (int i = 0; i < cleanInput.length(); i++) {
-        // Pop removes the top element (which is the last one added)
-        if (cleanInput.charAt(i) != stack.pop()) {
+// 3. Compare dequeue vs pop
+while (!stack.isEmpty() && !queue.isEmpty()) {
+        char popChar = stack.pop();        // LIFO: gets the last letter added
+        char dequeueChar = queue.remove(); // FIFO: gets the first letter added
+
+    if (popChar != dequeueChar) {
         isPalindrome = false;
         break; // Mismatch found, stop checking
         }
         }
 
-// 3. Print result
+// Print result
         if (isPalindrome) {
         System.out.println("Result: Success! '" + original + "' is a palindrome.");
 } else {
